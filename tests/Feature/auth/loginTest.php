98137bad8f743acc60c61a67049062023ef8d817
dato chanturia <datochanturia@redberry.ge>
2021-12-23 18:01:05 +0400

    test: logout test
    small fixes in login test

--- tests/Feature/auth/loginTest.php
+++ tests/Feature/auth/loginTest.php
@@ -21,14 +21,16 @@ class loginTest extends TestCase
 
 	public function test_user_can_login()
 	{
-		$user = User::factory()->create();
+		$user = User::factory()->create([
+			'username' => 'gela',
+			'password' => 'gela123',
+		]);
 
-		Livewire::actingAs($user)
-			->test(LoginPage::class)
-			->set('username', $user->username)
-			->set('password', $user->password)
-			->call('loginUser');
-		// ->assertRedirect(route('home'));
+		Livewire::test(LoginPage::class)
+			->set('username', 'gela')
+			->set('password', 'gela123')
+			->call('loginUser')
+			->assertRedirect(route('home'));
 
 		$this->assertAuthenticated();
 	}
@@ -43,5 +45,12 @@ class loginTest extends TestCase
 			->set('password', $user->password)
 			->call('loginUser')
 			->assertSee('your provided credentials could not be found');
+	}
+
+	public function test_logout_form_works()
+	{
+		$this->post(route('logout'));
+
+		$this->assertGuest();
 	}
 }
