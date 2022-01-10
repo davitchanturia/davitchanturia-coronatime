b2e42595140e204b2114984ef1143f4f5ea76422
dato chanturia <datochanturia@redberry.ge>
2022-01-10 18:30:54 +0400

    fix: testing schedule job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('create:generate-data')->daily();
+		$schedule->command('create:generate-data')->everyMinute();
 	}
 
 	/**
