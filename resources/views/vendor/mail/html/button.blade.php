bfb1cbb766f3c6eefc59d4caabad49abfd85fa3d
dato chanturia <datochanturia@redberry.ge>
2022-01-14 19:59:24 +0400

    style: markup for emails

---
+++ resources/views/vendor/mail/html/button.blade.php
@@ -0,0 +1,35 @@
+<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+  {{-- color: white;
+    background: #0FBA68;
+    font-weight: bold;
+    padding: 8px 15px;
+    text-decoration: none;
+    border-radius: 10px;
+    text-align: center;
+    
+    width: 50%;
+    display: block;
+    margin: auto;   --}}
+        <a 
+          style="color:white; background:#0FBA68; font-weight:bold; border-radius:9px; border:none; padding:8px 68px;"
+          href="{{ $url }}" class="button button-{{ $color ?? 'primary' }}" target="_blank" rel="noopener">{{ $slot }}
+        </a>
+
+
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
