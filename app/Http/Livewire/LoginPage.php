8744994155cfbf4e77cf5043cbd7fad3a1c1633f
dato chanturia <datochanturia@redberry.ge>
2022-01-15 13:08:40 +0400

    feat: user can login with username or email

--- app/Http/Livewire/LoginPage.php
+++ app/Http/Livewire/LoginPage.php
@@ -30,33 +30,69 @@ class LoginPage extends Component
 	{
 		$credentials = $this->validate();
 
-		$username = $credentials['username'];
-		$ifExistsInDatabase = User::where('username', $username)->first();
+		//checking input type email or username
+		$fieldType = filter_var($credentials['username'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
-		if ($ifExistsInDatabase)
+		if ($fieldType === 'email')
 		{
-			if ($ifExistsInDatabase->email_verified_at)
+			//checking if user exists
+			$username = $credentials['username'];
+			$ifExistsInDatabase = User::where('email', $username)->first();
+
+			if ($ifExistsInDatabase)
 			{
-				if (Auth::attempt($credentials, $this->remember))
+				if ($ifExistsInDatabase->email_verified_at)
 				{
-					return redirect(route('home', App::getLocale()));
+					$this->login('email');
 				}
 				else
 				{
-					throw ValidationException::withMessages([
-						'notFound' => 'your provided credentials is incorrect',
-					]);
+					return redirect(route('send.email', App::getLocale()));
 				}
 			}
 			else
 			{
-				return redirect(route('send.email', App::getLocale()));
+				throw ValidationException::withMessages([
+					'notFound' => 'your provided credentials could not be found',
+				]);
 			}
 		}
 		else
 		{
+			//checking if user exists
+			$username = $credentials['username'];
+			$ifExistsInDatabase = User::where('username', $username)->first();
+
+			if ($ifExistsInDatabase)
+			{
+				if ($ifExistsInDatabase->email_verified_at)
+				{
+					$this->login('username');
+				}
+				else
+				{
+					return redirect(route('send.email', App::getLocale()));
+				}
+			}
+			else
+			{
+				throw ValidationException::withMessages([
+					'notFound' => 'your provided credentials could not be found',
+				]);
+			}
+		}
+	}
+
+	public function login($type)
+	{
+		if (Auth::attempt([$type => $this->username, 'password' => $this->password], $this->remember))
+		{
+			return redirect(route('home', App::getLocale()));
+		}
+		else
+		{
 			throw ValidationException::withMessages([
-				'notFound' => 'your provided credentials could not be found',
+				'notFound' => 'your provided credentials is incorrect',
 			]);
 		}
 	}
